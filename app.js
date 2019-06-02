cb3bea1ef82fcb68e4f8e14a82a09f7535e26d5b
dc-2507 <dchaturvedi1_be16@thapar.edu>
2019-06-03 01:56:45 +0530

    add and remove functionality added

--- app.js
+++ app.js
@@ -14,20 +14,36 @@ console.log('Command : ',command);
 
 if(command === 'add')
 {
-    notes.addNote(argv.title, argv.body);
+    var note = notes.addNote(argv.title, argv.body);
+    var t = typeof note;
+    if(note){
+    console.log("Note added");
+    console.log("--");
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+    console.log("--");
+    }
+    else
+    console.log("Duplicate insertion");
 }
+
 else if(command === 'list')
 {
     notes.getAll();
 }
+
 else if(command === 'read')
 {
     notes.read(argv.title);
 }
+
 else if(command === 'remove')
 {
-    notes.remove(argv.title);
+    var noteRemoved = notes.remove(argv.title);
+    var message = noteRemoved ? 'Note was removed.': 'Note not found.' ;
+    console.log(message);
 }
+
 else
 {
     console.log('Command Not found.');
--- notes.js
+++ notes.js
@@ -1,5 +1,23 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
+var fetchNotes = () => {
+    try
+    {
+        var notesString=fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    }
+    catch(e)
+    {
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json',JSON.stringify(notes));
+};
+
 var getAll = () => {
     console.log('Getting all Notes.');
 };
@@ -9,12 +27,28 @@ var read = (title) => {
 }
 
 var remove = (title) => {
-    console.log('Removing note titled ',title);
+    var notes = fetchNotes();
+    var newNote = notes.filter((note) => note.title !== title);
+    saveNotes(newNote);
+
+    return notes.length !== newNote.length;
 }
 
 var addNote = (title, body) => {
-    console.log('Adding Note',title,body);
+    var notes = fetchNotes();
+    var note = {
+        title : title,
+        body : body
+    };
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if(duplicateNotes.length === 0){
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
 };
+
 module.exports = {
     addNote : addNote,
     getAll : getAll,
